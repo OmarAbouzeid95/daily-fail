e7ee8f0c94fcf92996fbe26192544dcf1ed2a4d2
Omar Abouzeid <omaradham1995@gmail.com>
2026-06-06 01:22:10 -0400

    chore: update db driver and ts configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FailModule } from './fail/fail.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    FailModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.DATABASE_URL as string,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
